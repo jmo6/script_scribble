a5c400994c9fa40202e91f627d233b325e1cbdcc
mcsquizzy123 <mcsquizzy123@bitbucket.org>
2017-10-13 19:24:08 -0700

    added 2 other java files we might use

---
+++ ScriptScribble/app/src/main/java/script/scribble/Button.java
@@ -0,0 +1,36 @@
+package script.scribble;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+public class Button {
+	Rect rect;
+	int x, y;
+	Paint background, font;
+	String text;
+	int padding;
+	
+	Button(int x, int y, Paint background, Paint font, String text, int padding) {
+		this.x = x;
+		this.y = y;
+		this.background = background;
+		this.font = font;
+		this.text = text;
+		this.padding = padding;
+
+		this.rect = new Rect();
+		this.font.getTextBounds(text, 0, text.length(), rect);
+		rect.offset(x, y);
+		
+		rect.left -= this.padding;
+		rect.top -= this.padding;
+		rect.right += this.padding;
+		rect.bottom += this.padding;
+	}
+	
+	void draw(Canvas canvas) {
+		canvas.drawRect(rect, background);
+		canvas.drawText(text, x, y, font);
+	}
+}
