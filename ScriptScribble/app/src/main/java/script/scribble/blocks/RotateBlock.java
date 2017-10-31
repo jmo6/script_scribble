03c66b1d04468f3f6084284184e620392486fb74
Jordan <jmo@ucsc.edu>
2017-10-30 18:17:26 -0700

    Added log tag to keep track of RotateBlock executing

--- ScriptScribble/app/src/main/java/script/scribble/blocks/RotateBlock.java
+++ ScriptScribble/app/src/main/java/script/scribble/blocks/RotateBlock.java
@@ -1,12 +1,15 @@
 package script.scribble.blocks;
 
 import android.graphics.Canvas;
+import android.util.Log;
 
 import script.scribble.BlockMenu;
 import script.scribble.CodingArea;
 
 public class RotateBlock extends Block {
     public int lastBlockRotateIndex;
+
+    private final String LOG_TAG =  "RotateBlock";
 
     public RotateBlock() {
         id = Block.ROTATE_BLOCK;
@@ -20,11 +23,12 @@ public class RotateBlock extends Block {
 
     @Override
     public void draw(Canvas canvas) {
-
+        //canvas.drawBitmap(imageIndex, x, y);
     }
 
     @Override
     public int execute(CodingArea codingArea) {
+        Log.d(LOG_TAG, "Rotate Block Executed");
         return 0;
     }
 }
